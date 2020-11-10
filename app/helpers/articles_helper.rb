f8f0691676d08ea8390a98419fa898321f855ef6
ShawnMorreau <Shawn.morreau@hotmail.com>
2020-11-10 16:35:13 -0500

    form-based workflow feature added

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
+
+    def flash_message(title, keyword)
+        return "Article '#{title}' has been #{keyword}"
+    end
 end
